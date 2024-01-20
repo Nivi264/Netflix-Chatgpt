49b1a0b18e43a60ecb265b452a7d3ed8b9bceb91
Nivi264 <tulasisreenivesh272@gmail.com>
2024-01-20 12:54:16 +0530

    gpt search disable if it is false in header is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,13 @@ import { addUser, removeUser } from '../utils/userSlice';
 import { LOGO, SUPPORTED_LANGUAGES } from '../utils/constant';
 import { toggleGptSearchView } from '../utils/gptSlice';
 import lang from '../utils/languageConstant';
+import { changeLanguage } from '../utils/configSlice';
 
 const Header = () => {
   const navigate=useNavigate();
   const dispatch=useDispatch();
   const user=useSelector(store=>store.user);
+  const showGptSearch=useSelector(store=>store.gpt.showGptSearch)
   const handleSignout=()=>{
     signOut(auth).then(() => {
       // Sign-out successful.
@@ -43,10 +45,13 @@ const Header = () => {
       //unsubscribe when component unmounts
       return()=>unsubscribe();
 },[dispatch]);
-
+ 
 const handleGptSearchClick=()=>{
   //toggle my gpt search
   dispatch(toggleGptSearchView())
+}
+const handleLanguageChange=(e)=>{
+  dispatch (changeLanguage(e.target.value))
 }
   return (
     <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between'>
@@ -54,9 +59,9 @@ const handleGptSearchClick=()=>{
         src={LOGO}
         alt='logo'/>
         {user && (<div className='flex p-2 '>
-          <select className='p-2 mx-2  my-5 bg-gray-900 text-white' >
+          {showGptSearch && (<select className='p-2 mx-2  my-5 bg-gray-900 text-white' onChange={handleLanguageChange} >
             {SUPPORTED_LANGUAGES.map(lang=> <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
-          </select>
+          </select>)}
           <button className='py-2 px-4   text-white bg-purple-800 mx-4 rounded-lg  my-5' onClick={handleGptSearchClick}>GPT Search</button>
           <img className='w-16 px-2 py-3 mx-2 my-2 right-2 p-10' alt='user icon' src={user?.photoURL}/>
           <button onClick={handleSignout} className=' left-1 font-bold text-white mx-2 my-2'>(Sign Out)</button>
